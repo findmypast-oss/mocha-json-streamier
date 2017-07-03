8003d27c06f07a01da430282b98a3500a5e602f8
David Elliott <dave.elliott@codevelop.co.uk>
2017-07-03 10:53:45 +0100

    PRO000: Added regex to extract module information from the stack trace.

---
+++ test/parse-stack-trace.spec.js
@@ -0,0 +1,30 @@
+const assert = require('assert');
+const { extractModuleLineAndColumn } = require('../lib/parse-stack-trace');
+
+describe('Works with a stack trace', function() {
+  it('should return a module name and line number from a stack trace.', function() {
+    const stackTrace = `
+    Error: Something unexpected has occurred.
+        at main (c:/Users/Me/Documents/MyApp/app.js:9:15)
+        at Object. (c:/Users/Me/Documents/MyApp/app.js:17:1)
+        at Module._compile (module.js:460:26)
+        at Object.Module._extensions..js (module.js:478:10)
+        at Module.load (module.js:355:32)
+        at Function.Module._load (module.js:310:12)
+        at Function.Module.runMain (module.js:501:10)
+        at startup (node.js:129:16)
+        at node.js:814:3`;
+    assert.deepEqual(extractModuleLineAndColumn(stackTrace), {
+      file: 'c:/Users/Me/Documents/MyApp/app.js',
+      line: 9,
+      column: 15,
+    });
+  });
+
+  it('should return an empty object if there is no stacktrace.', function() {
+    const stackTrace = `
+    Error: Something unexpected has occurred.
+        at node.js:814:3`;
+    assert.deepEqual(extractModuleLineAndColumn(stackTrace), {});
+  });
+});
